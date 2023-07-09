16c2e6d7d7fd0cc0b43ef523ab7c49e8f39f647a
TChandhar <tommyc90@hotmail.co.uk>
2023-07-09 15:01:55 +0100

    changed junit import to match version used to CalculatorTests now works

---
+++ src/test/java/com/TChandhar/calculator/CalculatorApplicationTest.java
@@ -0,0 +1,13 @@
+package com.TChandhar.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
