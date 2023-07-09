459dd1e0d45577e96a8789bf9e16639a7706fa6d
Tom <tommyc90@hotmail.co.uk>
2023-07-09 13:04:09 +0100

    Add Controller class and make changes to main app

--- src/main/java/com/TChandhar/calculator/CalculatorApplication.java
+++ src/main/java/com/TChandhar/calculator/CalculatorApplication.java
@@ -2,12 +2,22 @@ package com.TChandhar.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+	@Service
+	public class Calculator
+	{
+		public int sum( int a, int b)
+		{
+			return a+b;
+		}
 	}
 
 }
